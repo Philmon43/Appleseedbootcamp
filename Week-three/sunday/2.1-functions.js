3bae31c499cd7acac77421b93aa0f534b8cf5f54
philmon43 <philmon43@gmail.com>
2021-02-07 14:33:52 +0200

    javascript basic functions

---
+++ Week-three/sunday/2.1-functions.js
@@ -0,0 +1,5 @@
+const describeCountry = (country, population, capitalCity) => {
+    return `${country} has ${population} million of people and and it's Capitalcity is ${capitalCity}`
+}
+
+describeCountry
---
+++ Week-three/sunday/2.2-fortune-teller.js
@@ -0,0 +1,4 @@
+const tellFortune = (jobTitle, location, partnerName, numberOfChildren) => {
+    return `you will be ${jobTitle} in ${location} and married to ${partnerName} with ${numberOfChildren} children.`
+}
+
