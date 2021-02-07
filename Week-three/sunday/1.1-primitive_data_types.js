96d7dc0781d74e760c3e0504438500c6a81faddd
philmon43 <philmon43@gmail.com>
2021-02-07 13:20:17 +0200

    javascript primitive types

---
+++ Week-three/sunday/1.1-primitive_data_types.js
@@ -0,0 +1,15 @@
+const str = "string";
+const num = 10;
+const obj = {};
+const boo = true;
+const fName = num;
+const lName = undefined;
+const symType = Symbol();
+
+console.log(str);
+console.log(num);
+console.log(obj);
+console.log(boo);
+console.log(fName);
+console.log(lName);
+console.log(symType);
