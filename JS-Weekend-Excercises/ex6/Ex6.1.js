f452065a807097d8dff28ee307d17cdb3db2eb7a
philmon43 <philmon43@gmail.com>
2021-02-20 19:20:15 +0200

    accm

--- JS-Weekend-Excercises/ex6/Ex6.1.js
+++ JS-Weekend-Excercises/ex6/Ex6.1.js
@@ -7,11 +7,20 @@
 // accum("cwAt") -> "C-Ww-Aaa-Tttt"
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
 
-const accum = (str, idx = 0) => {
-    if (str.length == idx || !/^[a-zA-Z]*$/.test(str)) {
-        return ""
+const accum = (str) => {
+    let result = ''
+
+    function letter(str, idx = 0) {
+        if (str.length == idx || !/^[a-zA-Z]*$/.test(str)) {
+            return ""
+        }
+        return " " + str[idx].toUpperCase() + str[idx].toLowerCase().repeat(idx) + letter(str, idx += 1).replace(" ", "-");
     }
-    return " "+str[idx].toUpperCase()+str[idx].repeat(idx) + accum(str, idx+=1).replace(" ", "-");
+    
+    return result+=letter(str).trim()
 }
 
-console.log(accum("abcdefghijklmnopqrstuvwxyz"));
+
+// console.log(accum("abcdefghijklmnopqrstuvwxyz"));
+console.log(accum("abc"));
+console.log(accum("ZpglnRxqenU"));
