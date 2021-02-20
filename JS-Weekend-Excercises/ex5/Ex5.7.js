2c1eb02d68fc5add0ca80b0f46a5227e1c60e6fc
philmon43 <philmon43@gmail.com>
2021-02-20 18:28:51 +0200

    shortest words

--- JS-Weekend-Excercises/ex5/Ex5.7.js
+++ JS-Weekend-Excercises/ex5/Ex5.7.js
@@ -3,7 +3,7 @@
 // String will never be empty and you do not need to account for different data types.
 
 const shortestWord = (str) => {
-    return str.split(" ").reduce((a, b) => a.length < b.length ? a : b)
+    return str.split(" ").reduce((a, b) => a.length < b.length ? a : b).length
 }
 
 console.log(shortestWord("hello this is the Shortest word"))
