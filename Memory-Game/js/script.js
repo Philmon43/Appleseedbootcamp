2b802b26005954c87aba5c8f172cd096fd0b522b
philmon43 <philmon43@gmail.com>
2021-03-07 13:48:44 +0200

    memory game inital commit

---
+++ Memory-Game/js/script.js
@@ -0,0 +1,16 @@
+//shufle arr
+// const shufl = (arr) => {
+//     for (let i = arr.length - 1; i > 0; i--) {
+//         let x = Math.floor(Math.random() * (i + 1));
+//         let temp = arr[i];
+//         arr[i] = arr[x];
+//         arr[x] = temp;
+//     }
+//     return arr
+// }
+
+let level = "easy";
+
+const memoryGame = {
+    container: document.querySelector(".container"),
+}
