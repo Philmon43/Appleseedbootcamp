35508be1bd07a7cbc576ef915d4b255a4c253ffd
philmon43 <philmon43@gmail.com>
2021-03-09 19:18:10 +0200

    javascript clases and crud

---
+++ Javascript-todo-crud/todo-app-UI/crud.js
@@ -0,0 +1,95 @@
+const selectors = {
+    btnAdd: document.querySelector(".btn"),
+    addInput: document.querySelector(".addInput"),
+    listConatiner: document.querySelector(".my_list"),
+}
+
+class Todo {
+    constructor(task) {
+        this.id = 0;
+        this.task = task
+        this.isCompleted = false
+        this.timeCreated = Date.now()
+    }
+}
+class List {
+    constructor() {
+        this.arr = JSON.parse(localStorage.getItem("data")) || [];
+    }
+    addNew(task) {
+        try {
+            if (task === "") throw ("error")
+            let newTask = new Todo(task)
+            newTask.id = this.arr.length + 1
+            this.arr.push(newTask)
+            this.sync()
+        } catch (e) {
+            return e
+        }
+    }
+    remove(id) {
+        this.arr = this.arr.filter(a => a.id !== id);
+        this.sync()
+    }
+    update(id, task) {
+        this.arr = this.arr.filter(a => {
+            if (id === a.id) {
+                return a.task = task
+            }
+            return a
+        });
+        this.sync()
+    }
+    sync() {
+        localStorage.setItem("data", JSON.stringify(this.arr));
+    }
+}
+const todo = new List()
+
+const render = () => {
+    try {
+        const data = JSON.parse(localStorage.getItem("data"))
+        console.log(data)
+        selectors.listConatiner.innerText = "";
+        data.forEach(data => {
+            let div = document.createElement("div");
+            let detail = document.createElement("div")
+            let task = document.createElement("h2");
+            let time = document.createElement("p")
+            let updInput = document.createElement("input");
+            let del = document.createElement("button");
+            let update = document.createElement("button");
+            detail.appendChild(task)
+            detail.appendChild(time)
+            detail.appendChild(updInput)
+            div.appendChild(detail)
+            div.appendChild(update)
+            div.appendChild(del)
+            del.innerText = "Delete"
+            update.innerText = "Update"
+            task.innerText = data.task
+            time.textContent = "Created: " + new Date(data.timeCreated).toTimeString();
+            del.classList.add("del");
+            update.classList.add("update");
+            del.setAttribute("data-id", data.id);
+            update.setAttribute("data-id", data.id);
+            div.classList.add("box");
+            detail.classList.add("detail");
+            updInput.classList.add("innerInput");
+            updInput.classList.add("input-" + data.id);
+            updInput.placeholder = data.task
+            selectors.listConatiner.appendChild(div)
+        });
+    } catch (e) {
+        return e
+    }
+}
+render()
+//add new todo
+const addNewTodo = () => {
+    const task = selectors.addInput.value;
+    todo.addNew(task)
+    render()
+}
+
+selectors.btnAdd.addEventListener("click", addNewTodo);
